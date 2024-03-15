4ccd32d0fea205885c7c0583856c8c03f1c05afe
Prabhudutta Panigrahi <58845544+p-panigrahi@users.noreply.github.com>
2024-03-15 20:43:44 +0530

    Clouser Complete

---
+++ Clouser/Clouser.js
@@ -0,0 +1,27 @@
+// First
+// function outerFun() {
+//   var msg = "Outer Message";
+//   function innerFun() {
+//     var outerMsg = msg;
+//     var innerMsg = "Inner Message";
+//     return outerMsg + innerMsg;
+//   }
+//   var Result = innerFun();
+//   console.log(Result);
+// }
+// outerFun();
+
+// Second
+function add(a, b, c) {
+  return {
+    getSumTwo: function () {
+      return a + b;
+    },
+    getSumThree: function () {
+      return a + b + c;
+    },
+  };
+}
+var sum = add(2, 3, 4);
+console.log(sum.getSumThree());
+console.log(sum.getSumTwo());
